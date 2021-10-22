520393c75ff7b9dc27253acd0ea0ce8565d0ec3b
Daniel Mateus X Silva <danielmxsilva@gmail.com>
2021-10-22 19:21:20 -0300

    HEADER/FOOTER/E HEADER-HOME PRONTO

--- pages/home.php
+++ pages/home.php
@@ -1,1 +1,23 @@
-<h1>Ola da home</h1>
+<section class="home-header bg-header">
+	<div class="container">
+		<div class="flex-box">
+			<div class="w50">
+				<h2>loren ipsum sit dolor amet ipsum sit dolor ipsum <span class="span-home">sit dolor</span></h2>
+				<p>loren ipsum sit dolor amet. ipsummit dolor ipsum</p>
+				<div class="lista-header">
+					<div><i class="fa fa-check"></i><span>loren ipsum sit dolor</span></div>
+					<div><i class="fa fa-check"></i><span>loren ipsum sit dolor</span></div>
+					<div><i class="fa fa-check"></i><span>loren ipsum sit dolor</span></div>
+					<div><i class="fa fa-check"></i><span>loren ipsum sit dolor</span></div>
+				</div><!--lista-header-->
+				<div class="btn-rosa">
+					<a href="#">COMPRAR AGORA</a>
+				</div><!--btn-rosa-->
+			</div><!--w50-->
+			<div class="w50">
+				<img src="<?php echo INCLUDE_PATH?>img/header/mockup-header.png">
+			</div><!--w50-->
+		</div><!--flex-box-->
+		<div class="logo-home"><img src="<?php echo INCLUDE_PATH?>img/header/logo-mockup.png"></div>
+	</div><!--container-->
+</section>
