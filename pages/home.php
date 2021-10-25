e62d87cf1d22a3cad756788db23ad01b3f9d8a05
Daniel Mateus X Silva <danielmxsilva@gmail.com>
2021-10-25 15:14:14 -0300

    FRONT-END PRONTO SÓ FALTA APLICAR O JS

--- pages/home.php
+++ pages/home.php
@@ -21,3 +21,265 @@
 		<div class="logo-home"><img src="<?php echo INCLUDE_PATH?>img/header/logo-mockup.png"></div>
 	</div><!--container-->
 </section>
+
+<section class="depoimentos-resultados">
+	<div class="container">
+		<h2>DEPOIMENTOS & RESULTADOS</h2>
+		<div class="flex-box">
+			<div class="w50">
+				<div class="depoimento-wraper">
+					<div class="depoimento-single">
+						<div class="img-depoimento-single">
+							<img src="<?php echo INCLUDE_PATH?>img/depoimentos/globo.png">
+						</div><!--img-depoimento-single-->
+						<p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam quis ultricies libero. Integer consectetur condimentum fermentum. Cras sit amet libero vel tortor luctus posuere sit amet ut nulla. Etiam vestibulum mollis turpis non facilisis. Aliquam a nulla eget lacus aliquam viverra scelerisque in lacus. Sed in nisi sit amet metus luctus hendrerit. Suspendisse ac velit velit. Etiam sed fermentum nisl.
+Mauris at lorem quam. Ut at diam sit amet risus consectetur scelerisque id eu nulla. Vestibulum accumsan ac nulla at viverra"</p>
+					</div><!--depoimento-single-->
+				</div><!--depoimento-wraper-->
+			</div><!--w50-->
+			<div class="w50">
+				<div class="box-resultados">
+					<div class="img-resultado-1">
+						<img src="<?php echo INCLUDE_PATH?>img/depoimentos/depoimento1.png">
+					</div><!--img-resultado-1-->
+					<div class="img-resultado-2">
+						<img src="<?php echo INCLUDE_PATH?>img/depoimentos/depoimento3.png">
+					</div><!--img-resultado-2-->
+					<div class="img-resultado-3">
+						<img src="<?php echo INCLUDE_PATH?>img/depoimentos/depoimento4.png">
+					</div><!--img-resultado-3-->
+				</div><!--box-resultados-->
+			</div><!--w50-->
+		</div><!--flex-box-->
+	</div><!--container-->
+</section><!--depoimentos-resultados-->
+
+<section class="vantagens">
+	<div class="container">
+
+		<div class="vantagens-header">
+			<h2>VANTAGENS</h2>
+			<p>LOREN IPSUM SIT DOLLOR AMET</p>
+		</div><!--vantagens-header-->
+
+		<div class="vantagens-conteudo">
+			<div class="flex-box">
+
+				<div class="w33">
+					<div class="vantagens-single">
+						<i class="fas fa-clock"></i>
+						<h2>LOREN IPSUM</h2>
+						<p>Loren ipsum dolor sit amet, consectetur adisicing elit. Eliam a visit sit amet augus</p>
+					</div><!--vantagens-single-->
+				</div><!--w33-->
+
+				<div class="w33">
+					<div class="vantagens-single">
+						<i class="fas fa-clock"></i>
+						<h2>LOREN IPSUM</h2>
+						<p>Loren ipsum dolor sit amet, consectetur adisicing elit. Eliam a visit sit amet augus</p>
+					</div><!--vantagens-single-->
+				</div><!--w33-->
+
+				<div class="w33">
+					<div class="vantagens-single">
+						<i class="fas fa-clock"></i>
+						<h2>LOREN IPSUM</h2>
+						<p>Loren ipsum dolor sit amet, consectetur adisicing elit. Eliam a visit sit amet augus</p>
+					</div><!--vantagens-single-->
+				</div><!--w33-->
+
+				<div class="w33">
+					<div class="vantagens-single">
+						<i class="fas fa-clock"></i>
+						<h2>LOREN IPSUM</h2>
+						<p>Loren ipsum dolor sit amet, consectetur adisicing elit. Eliam a visit sit amet augus</p>
+					</div><!--vantagens-single-->
+				</div><!--w33-->
+
+				<div class="w33">
+					<div class="vantagens-single">
+						<i class="fas fa-clock"></i>
+						<h2>LOREN IPSUM</h2>
+						<p>Loren ipsum dolor sit amet, consectetur adisicing elit. Eliam a visit sit amet augus</p>
+					</div><!--vantagens-single-->
+				</div><!--w33-->
+
+				<div class="w33">
+					<div class="vantagens-single">
+						<i class="fas fa-clock"></i>
+						<h2>LOREN IPSUM</h2>
+						<p>Loren ipsum dolor sit amet, consectetur adisicing elit. Eliam a visit sit amet augus</p>
+					</div><!--vantagens-single-->
+				</div><!--w33-->
+
+
+			</div><!--flex-box-->
+		</div><!--vantagens-conteudo-->
+	</div><!--container-->
+</section><!--vantagens-->
+
+<section class="quem-usa-aprova">
+	<div class="container">
+
+		<div class="vantagens-header">
+			<h2>QUEM USA APROVA</h2>
+			<p>LOREN IPSUM SIT DOLLOR AMET</p>
+		</div><!--vantagens-header-->
+
+<?php /* ?>
+		<div class="box-video-wraper">
+
+			<div class="video-single">
+				<video>
+					<source src="<?php echo INCLUDE_PATH?>img/quem-usa/VID-20210713-WA0009.mp4" type="video/mp4">
+				</video>
+			</div><!--video-single-->
+
+			<div class="video-single">
+				<video>
+					<source src="<?php echo INCLUDE_PATH?>img/quem-usa/VID-20210715-WA0526.mp4" type="video/mp4">
+				</video>
+			</div><!--video-single-->
+
+			<div class="video-single">
+				<video>
+					<source src="<?php echo INCLUDE_PATH?>img/quem-usa/VID-20210821-WA0064.mp4" type="video/mp4">
+				</video>
+			</div><!--video-single-->
+
+		</div><!--box-video-wraper-->
+<?php */?>
+
+	</div><!--container-->
+</section><!--quem-usa-aprova-->
+
+<section class="kit-compra">
+	<div class="container">
+
+		<div class="flex-box">
+
+			<div class="kit-single-wraper">
+				<h3>KIT 1</h3>
+				<div class="kit-single">
+					<div class="kit-img-single">
+						<img src="<?php echo INCLUDE_PATH?>img/header/mockup-single.png">
+					</div><!--kit-img-single-->
+					<p><b>Leve apenas 1</b></p>
+					<p class="align-p">Por <b>apenas</b> <span class="align-txt"><span class="line-red"></span>R$197,00</p></span>
+					<p><b>12x R$14,76</b></p>
+					<p>Valor à vista de <br/> R$147,76 (15% OFF)</p>
+					<p><b>Frete Grátis</b></p>
+					<div class="btn-rosa">
+						<a href="#">COMPRAR AGORA</a>
+					</div><!--btn-rosa-->
+				</div><!--kit-single-->
+			</div><!--kit-single-wraper-->
+
+			<div class="kit-single-wraper">
+				<h3>KIT 2</h3>
+				<div class="kit-single">
+					<div class="kit-img-single">
+						<img src="<?php echo INCLUDE_PATH?>img/header/mockup-triplo.png">
+					</div><!--kit-img-single-->
+					<p><b>Compre 2 leve 3</b></p>
+					<p class="align-p">Por <b>apenas</b> <span class="align-txt"><span class="line-red"></span>R$197,00</p></span>
+					<p><b>12x R$14,76</b></p>
+					<p>Valor à vista de <br/> R$147,76 (15% OFF)</p>
+					<p><b>Frete Grátis</b></p>
+					<div class="btn-rosa">
+						<a href="#">COMPRAR AGORA</a>
+					</div><!--btn-rosa-->
+				</div><!--kit-single-->
+			</div><!--kit-single-wraper-->
+
+			<div class="kit-single-wraper">
+				<h3>KIT 3</h3>
+				<div class="kit-single">
+					<div class="kit-img-single">
+						<img src="<?php echo INCLUDE_PATH?>img/header/mockup-quatro.png">
+					</div><!--kit-img-single-->
+					<p><b>Compre 3 leve 4</b></p>
+					<p class="align-p">Por <b>apenas</b> <span class="align-txt"><span class="line-red"></span>R$197,00</p></span>
+					<p><b>12x R$14,76</b></p>
+					<p>Valor à vista de <br/> R$147,76 (15% OFF)</p>
+					<p><b>Frete Grátis</b></p>
+					<div class="btn-rosa">
+						<a href="#">COMPRAR AGORA</a>
+					</div><!--btn-rosa-->
+				</div><!--kit-single-->
+			</div><!--kit-single-wraper-->
+
+
+		</div><!--flex-box-->
+
+	</div><!--container-->
+	<div class="img-wraper-kit">
+		<img src="<?php echo INCLUDE_PATH?>img/image-kit-envio.png">
+	</div><!--img-wraper-kit-->
+</section><!--kit-compra-->
+
+<section class="garantia">
+	<div class="container">
+		<div class="flex-box">
+			<div class="img-wraper-garantia">
+				<img src="<?php echo INCLUDE_PATH?>img/garantia.png">
+			</div><!--img-wraper-garantia-->
+			<div class="txt-wraper-garantia">
+				<h2>Loren Ipsum Sit Dolor Amet Ipsum</h2>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam quis ultricies libero. Integer consectetur condimentum fermentum. Cras sit amet libero vel tortor luctus posuere sit amet ut nulla. Etiam vestibulum mollis turpis non facilisis. Aliquam a nulla eget lacus aliquam viverra scelerisque</p>
+				<div class="btn-rosa garantia-btn">
+					<a href="#">COMPRAR AGORA</a>
+				</div><!--btn-rosa-->
+			</div><!--txt-wraper-garantia-->
+		</div><!--flex-box-->
+	</div><!--container-->
+</section><!--garantia-->
+
+<section class="section-faq">
+	<div class="container">
+		<div class="vantagens-header">
+			<h2>Loren Ipsum Sit Dolor Amet Ipsum</h2>
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam quis ultricies libero. Integer consectetur condimentum fermentum. Cras sit amet libero vel tortor luctus posuere sit amet ut nulla</p>
+		</div><!--vantagens-header-->
+
+		<div class="wraper-faq">
+
+			<div class="faq-single">
+				<div class="faq-pergunta">
+					<span class="faq-sinal">+</span>
+					<h3>loren ipsum dolor sit amer consectetur?</h3>
+				</div><!--faq-pergunta-->
+				<div class="faq-resposta">
+					<p>Loren ipsum Dolor Sit Amet. Consectetur Adiscipling Elit Mascenas Imperdie</p>
+				</div><!--faq-resposta-->
+			</div><!--faq-single-->
+
+			<div class="faq-single">
+				<div class="faq-pergunta">
+					<span class="faq-sinal">+</span>
+					<h3>loren ipsum dolor sit amer consectetur?</h3>
+				</div><!--faq-pergunta-->
+				<div class="faq-resposta">
+					<p>Loren ipsum Dolor Sit Amet. Consectetur Adiscipling Elit Mascenas Imperdie</p>
+				</div><!--faq-resposta-->
+			</div><!--faq-single-->
+
+			<div class="faq-single">
+				<div class="faq-pergunta">
+					<span class="faq-sinal">+</span>
+					<h3>loren ipsum dolor sit amer consectetur?</h3>
+				</div><!--faq-pergunta-->
+				<div class="faq-resposta">
+					<p>Loren ipsum Dolor Sit Amet. Consectetur Adiscipling Elit Mascenas Imperdie</p>
+				</div><!--faq-resposta-->
+			</div><!--faq-single-->
+
+		</div><!--wraper-faq-->
+
+		<div class="btn-rosa">
+			<a href="#">COMPRAR AGORA</a>
+		</div><!--btn-rosa-->
+
+	</div><!--container-->
+</section><!--section-faq-->
